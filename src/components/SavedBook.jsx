6d7d6df88f43d78f993148a55aee59d04adca871
Stephanie-17 <Stephanie>
2025-08-08 23:36:05 +0100

    made changes

--- src/components/SavedBook.jsx
+++ src/components/SavedBook.jsx
@@ -5,7 +5,7 @@ const SavedBook = () => {
     <div className='mx-7 my-3'>
         <img className='w-[200px] h-[250px] rounded-lg' src={coverImg} alt="coverImg" />
         <div>
-            <p className='font-semibold'>Atomic Habits on food</p>
+            <p className='font-semibold'>Atomic Habits</p>
             <p className='text-[#1747B5]'>James Clear</p>
         </div>
     </div>
