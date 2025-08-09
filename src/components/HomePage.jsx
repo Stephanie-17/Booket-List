b5b23219c9f6a1b6d4b6b5648333e649caf51127
Stephanie-17 <Stephanie>
2025-08-09 23:25:05 +0100

    changed background image

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,11 +1,11 @@
 
-import heroImg from '../assets/homepage.jpg'
+import heroImg from '../assets/try.png'
 
 const HomePage = ({setCurrentPage}) => {
   return (
     <div className='flex justify-center items-center mb-30'>
       <div className='flex justify-center items-center flex-col mt-8' >
-         <img  className='w-[80%]' src={heroImg} alt="" />
+         <img  className='w-[70%]' src={heroImg} alt="" />
          <div className='text-center text-white'>
             <h2 className='text-5xl font-semibold -mt-92'>Find Your Next Favorite Book</h2>
             <p>Discover personalized book recommendations based on your reading history. Start exploring today!</p>
